7e1157819b2bd2d9a23e1c07a13980e6e3bc34da
EFisher828 <71984782+EFisher828@users.noreply.github.com>
2022-06-06 16:57:25 -0400

    Add files via upload

---
+++ locations.js
@@ -0,0 +1,58 @@
+const locDic = {
+  "Private Property - 3800'": {
+    'link': './images/Private 5-27-22.jpg',
+    'date': 'May 27, 2022',
+    'photog': 'Evan Fisher',
+    'handle': 'https://twitter.com/EFisherWX',
+    'loc': [35.5564, -82.4527]
+  },
+  "Inspiration Point - 4700'": {
+    'link': './images/Inspiration Point 5-30-22.jpg',
+    'date': 'May 30, 2022',
+    'photog': 'Billy Bowling',
+    'handle': 'https://twitter.com/babowling12',
+    'loc': [35.6361, -83.4424]
+  },
+  "AT at Grayson Highlands - 5000'": {
+    'link': './images/Grayson Highlands 6-4-22.jpg',
+    'date': 'June 4, 2022',
+    'photog': 'Billy Bowling',
+    'handle': 'https://twitter.com/babowling12',
+    'loc': [36.6689, -81.5101]
+  },
+  "Craggy Gardens - 5500'": {
+    'link': './images/Craggy Gardens 6-4-22.jpg',
+    'date': 'June 4, 2022',
+    'photog': 'Evan Fisher',
+    'handle': 'https://twitter.com/EFisherWX',
+    'loc': [35.7003, -82.3796]
+  },
+  "UNC Asheville - 2100'": {
+    'link': './images/Null UNCA 6-5-22.jpg',
+    'date': 'June 5, 2022',
+    'photog': 'Kyle Noel',
+    'handle': 'https://twitter.com/KyleNoelWx',
+    'loc': [35.6162, -82.5662]
+  },
+  "Paris Mountain State Park - ~1000'": {
+    'link': './images/Paris Mountain State Park 6-5-22.jpg',
+    'date': 'June 5, 2022',
+    'photog': 'Jason Greer',
+    'handle': 'https://twitter.com/Jasongreer',
+    'loc': [34.9269, -82.3722]
+  },
+  "Craggy Gardens Picnic Area - 5200'": {
+    'link': './images/Craggy Picnic Area 6-6-22.jpg',
+    'date': 'June 6, 2022',
+    'photog': 'Jack Kendrick',
+    'handle': 'https://twitter.com/jackendrickwx',
+    'loc': [35.6998, -82.3911]
+  },
+  "Bearwallow Mountain - 4200'": {
+    'link': './images/Bearwallow 6-6-22.jpg',
+    'date': 'June 6, 2022',
+    'photog': 'Jack Kendrick',
+    'handle': 'https://twitter.com/jackendrickwx',
+    'loc': [35.4636, -82.3598]
+  },
+}
